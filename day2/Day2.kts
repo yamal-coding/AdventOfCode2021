744063b143a04b2b850c2ed93dd7244beafaa317
yamal <yamal.coding@gmail.com>
2021-12-07 16:30:04 +0100

    part one of day 2

---
+++ day2/Day2.kts
@@ -0,0 +1,32 @@
+#!/usr/bin/env kotlin
+
+import java.io.File
+import java.util.Scanner
+
+/*
+ * Part 1 of day 2: https://adventofcode.com/2021/day/2
+ */
+
+val file = File("day_2_input.txt")
+val scanner = Scanner(file.inputStream())
+
+var moreMeasurements = true
+var x = 0
+var y = 0
+
+while (moreMeasurements) {
+	try {
+		val direction = scanner.next()
+		val move = scanner.nextInt()
+
+		when (direction) {
+			"forward" -> x += move
+			"up" -> y -= move
+			"down" -> y += move
+		}
+	} catch (e: NoSuchElementException) {
+		moreMeasurements = false
+	}
+}
+
+println("X: $x; Y: $y; X * Y: ${x * y}")
