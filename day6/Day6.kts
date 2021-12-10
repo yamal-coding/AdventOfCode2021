8ad3aa3be98e5182f94de945161b84215a8497ec
yamal <yamal.coding@gmail.com>
2021-12-10 11:44:15 +0100

    day 6 part one and part two optimazed code

--- day6/Day6.kts
+++ day6/Day6.kts
@@ -4,29 +4,43 @@ import java.io.File
 import java.util.Scanner
 
 /*
- * Part 1 of day 6: https://adventofcode.com/2021/day/6
+ * Day 6: https://adventofcode.com/2021/day/6
+ * Code for day 6 is the same for part 1 and part 2. 
+ * This problem was solved using dynamic programming where I'm caching
+ * already processed fishes with a specific timer for a specific numer of remaining days.
  */
 
 val file = File("day_6_input.txt")
 val scanner = Scanner(file.inputStream())
 
-var daysLeft = 80
-val lanternFishes = scanner.nextLine().split(",").map { it.toInt() }.toMutableList()
+var DAYS = 256
+val lanternFishes = scanner.nextLine().split(",").map { it.toInt() }
+var total = 0L
+val childrenOfAlreadyProcessedFishes = mutableMapOf<Pair<Int, Int>, Long>()
 
-while (daysLeft > 0) {
-	val numOfFishes = lanternFishes.size
-
-	for (i in 0 until numOfFishes) {
-		val timer = lanternFishes[i] - 1
-		if (timer < 0) {
-			lanternFishes[i] = 6
-			lanternFishes.add(8)
-		} else {
-			lanternFishes[i] = timer
-		}
-	}
-
-	daysLeft--
+lanternFishes.forEach { initialTimer ->
+	total += 1 + calculateChildren(DAYS, initialTimer)
 }
 
-println("Num of lantern fishes: ${lanternFishes.size}")
+fun calculateChildren(remainingDays: Int, timer: Int): Long {
+	val childrenOfAlreadyProcessedFish = childrenOfAlreadyProcessedFishes[timer to remainingDays]
+	if (childrenOfAlreadyProcessedFish != null) {
+		return childrenOfAlreadyProcessedFish
+	}
+
+	var children = 0L
+
+	if (remainingDays > timer) {
+		children = 1L + ((remainingDays - timer - 1) / 7)
+	}
+
+	for (i in 0L until children) {
+		children += calculateChildren(remainingDays - timer - i.toInt() * 7, 9)
+	}
+
+	childrenOfAlreadyProcessedFishes[timer to remainingDays] = children
+
+	return children
+}
+
+println("Num of lantern fishes: $total")
