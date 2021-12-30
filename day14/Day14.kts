e861156c0a5576b3ce892bca75fbb39180be0ca6
yamal <yamal.coding@gmail.com>
2021-12-30 01:41:39 +0100

    part two of day 14

--- day14/Day14.kts
+++ day14/Day14.kts
@@ -3,52 +3,87 @@
 import java.io.File
 import java.util.LinkedList
 import java.util.Scanner
+import kotlin.text.single
 
 /*
- * Part 1 of day 14: https://adventofcode.com/2021/day/14
+ * Day 14: https://adventofcode.com/2021/day/14
+ * Code for day 14 is the same for part 1 and part 2.
  */
 
 val file = File("day_14_input.txt")
 val scanner = Scanner(file.inputStream())
 
-val rules = mutableMapOf<String, String>()
+val rules = mutableMapOf<String, Char>()
 
-val template = scanner.next()
-val polymer = LinkedList<String>()
-val counters = mutableMapOf<String, Int>()
-template.forEach {
-	val element = it.toString()
-	polymer.add(element)
-	counters[element] = (counters[element] ?: 0) + 1
+val template = scanner.next().map { it }
+val globalCounters = mutableMapOf<Char, Long>()
+val alreadyCalculatedTuplesWithCounter = mutableMapOf<Pair<Int, String>, Map<Char, Long>>()
+template.forEach { element ->
+	globalCounters[element] = (globalCounters[element] ?: 0L) + 1L
 }
 
 while (scanner.hasNext()) {
 	val rulePattern = scanner.next()
 	scanner.next() // read "->" token
-	val ruleElement = scanner.next()
+	val ruleElement = scanner.next().single()
 
 	rules[rulePattern] = ruleElement
 }
 
-val steps = 10
-for (step in 1..steps) {
-	val iterator = polymer.listIterator()
-    while (iterator.hasNext()) {
-		val firstElement = iterator.next()
-		if (iterator.hasNext()) {
-			val secondElement = iterator.next()
-			val pattern = firstElement + secondElement
+val steps = 40
+for (i in 0 until template.size - 1) {
+	val firstElement = template[i]
+	val secondElement = template[i + 1]
 
-			rules[pattern]?.let { newElement ->
-				iterator.previous()
-				iterator.add(newElement)
-				counters[newElement] = (counters[newElement] ?: 0) + 1
+	val counters = generatePolymerFromTuple(steps, firstElement, secondElement)
+
+	increaseCounter(globalCounters, counters)
+}
+
+fun generatePolymerFromTuple(step: Int, firstElement: Char, secondElement: Char): Map<Char, Long> {
+	if (step == 0) {
+		return emptyMap()
+	}
+
+	val counters = mutableMapOf<Char, Long>()
+
+	val pattern = "$firstElement$secondElement"
+	rules[pattern]?.let { newElement ->
+		counters[newElement] = (counters[newElement] ?: 0L) + 1L
+
+		val firstPattern = "$firstElement$newElement"
+		if (alreadyCalculatedTuplesWithCounter[step - 1 to firstPattern] == null) {
+			val tempCounters = generatePolymerFromTuple(step - 1, firstElement, newElement)
+			increaseCounter(counters, tempCounters)
+		} else {
+			alreadyCalculatedTuplesWithCounter[step - 1 to firstPattern]?.let { alreadyCalculatedCounters ->
+				increaseCounter(counters, alreadyCalculatedCounters)
 			}
 		}
+
+		val secondPattern = "$newElement$secondElement"
+		if (alreadyCalculatedTuplesWithCounter[step - 1 to secondPattern] == null) {
+			val tempCounters = generatePolymerFromTuple(step - 1, newElement, secondElement)
+			increaseCounter(counters, tempCounters)
+		} else {
+			alreadyCalculatedTuplesWithCounter[step - 1 to secondPattern]?.let { alreadyCalculatedCounters ->
+				increaseCounter(counters, alreadyCalculatedCounters)
+			}
+		}
+
+		alreadyCalculatedTuplesWithCounter[step to pattern] = counters.toMutableMap()
+	}
+
+	return counters
+}
+
+fun increaseCounter(base: MutableMap<Char, Long>, increase: Map<Char, Long>) {
+	increase.forEach {
+		base[it.key] = (base[it.key] ?: 0L) + it.value
 	}
 }
 
-val maxRepetitions = counters.maxByOrNull { it.value }!!.value
-val minRepetitions = counters.minByOrNull { it.value }!!.value
+val maxRepetitions = globalCounters.maxByOrNull { it.value }!!.value
+val minRepetitions = globalCounters.minByOrNull { it.value }!!.value
 
 println("Repetitions of most common element minus repetitions of least common element: $maxRepetitions - $minRepetitions = ${maxRepetitions - minRepetitions}")
