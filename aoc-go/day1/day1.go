543d8cb9e013a5082f4bf911a3719ea08ca1099b
yamal <yamal.coding@gmail.com>
2022-01-07 14:45:44 +0100

    part one of day 1 in go

---
+++ aoc-go/day1/day1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, openError := os.Open("day_1_input.txt")
+	check(openError)
+
+	scanner := bufio.NewScanner(file)
+	lastMeasurement := 0
+	measurementsLargerThanPreviousOne := -1
+
+	for scanner.Scan() {
+		newMeasurementStr := scanner.Text()
+		newMeasurement, parseError := strconv.Atoi(newMeasurementStr)
+		check(parseError)
+
+		if newMeasurement > lastMeasurement {
+			measurementsLargerThanPreviousOne = measurementsLargerThanPreviousOne + 1
+		}
+		lastMeasurement = newMeasurement
+	}
+
+	file.Close()
+	fmt.Printf("Measurements larger than previous one: %d\n", measurementsLargerThanPreviousOne)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
